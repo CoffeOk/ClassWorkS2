0f3c10612a6d4f295f73f63847e64e02369fb7db
Wall-e <you@mail.com>
2023-09-28 00:43:53 +0300

    Reshenie 2 zadachi 12

--- Task03(12)/Program.cs
+++ Task03(12)/Program.cs
@@ -2,15 +2,23 @@
 // На выходе - кратно ли первое число второму
 // Если нет - вывести остаток от деления
 
-int a, b;
+int a, b; 
+
+int OstatokDelenia(int x, int y)
+{
+   int z = x % y;
+   return z; 
+} 
+
 Console.Write("Введите число a: ");
 while (!int.TryParse(Console.ReadLine(), out a))
     Console.WriteLine("Введите число a: ");
 Console.Write("Введите число b: ");
 while (!int.TryParse(Console.ReadLine(), out b))
     Console.WriteLine("Введите число b: ");
-int x = a % b;
+// int x = a % b;
+int c = OstatokDelenia (a, b);
 if (a % b == 0)
     Console.WriteLine($"{a}, {b} -> Кратно");
 else
-    Console.WriteLine($"{a}, {b} -> Не кратно, остаток {x}");
+    Console.WriteLine($"{a}, {b} -> Не кратно, остаток {c}");
