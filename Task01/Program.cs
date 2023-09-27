cee8bbda029a965d3768871c5457dca363aeb756
Wall-e <you@mail.com>
2023-09-27 22:07:45 +0300

    Reshenie 1 zadachi 9

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ввод: Рандом от 10 до 99
+//Вывод: Найбольшая цифра числа
+
+
+//Решение 1.
+
+int num = new Random().Next(10,100);
+//Console.Write($"Случайное число -> {num}");
+int a = num / 10;
+int b = num % 10;
+if (a > b)
+{
+    Console.Write($"{num} - > {a}");
+}
+else
+{
+    Console.Write($"{num} - > {b}");
+}
