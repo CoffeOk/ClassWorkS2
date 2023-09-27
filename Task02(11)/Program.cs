7231be4c433fc533f23800fdc1607743f3860350
Wall-e <you@mail.com>
2023-09-27 23:45:54 +0300

    Reshenie 2 zadachi 11

--- Task02(11)/Program.cs
+++ Task02(11)/Program.cs
@@ -1,8 +1,21 @@
 ﻿//Ввод: Рандом от 100 до 1000
 //Вывод: Число с удалённым десятком
 
+using System.Runtime.InteropServices.ComTypes;
 
 int num = new Random().Next(100, 1000);
+
+int DeleteDes (int numb)
+{
+    int sot = numb % 10;
+    int ed = numb / 100;
+    return (ed * 10 +sot);
+}
+
+int res = DeleteDes (num);
+Console.Write ($"{num} -> {res}");
+/* 
 int sot = num % 10;
 int ed = num / 100;
-Console.Write ($"{num} -> {ed}{sot}");  
+Console.Write ($"{num} -> {ed}{sot}");   
+*/
