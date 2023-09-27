2b45e21b1b88657792ff3e539aec47a7dd1724e4
Wall-e <you@mail.com>
2023-09-27 22:51:13 +0300

    Reshenie 2 zadachi 9 - cherez metod

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -4,8 +4,9 @@
 
 //Решение 1.
 
-int num = new Random().Next(10,100);
+ int num = new Random().Next(10,100);
 //Console.Write($"Случайное число -> {num}");
+/*
 int a = num / 10;
 int b = num % 10;
 if (a > b)
@@ -20,4 +21,28 @@ else
 //Ternarnii operator
 
 int maxDigit = a > b ? a : b;
-Console.WriteLine($"{num} -> {maxDigit}");
+Console.WriteLine($"{num} -> {maxDigit}"); 
+*/
+
+//Reshenir cherez metod
+
+int maxDigit1 = MaxDigit(num);
+Console.WriteLine($"{num} -> {maxDigit1}");
+int maxDigit2 = MaxDigit(12);
+Console.WriteLine($"max -> {maxDigit2}");
+int maxDigit3 = MaxDigit(13);
+Console.WriteLine($"max -> {maxDigit3}");
+int maxDigit4 = MaxDigit(77);
+Console.WriteLine($"max -> {maxDigit4}");
+int maxDigit5 = MaxDigit(56);
+Console.WriteLine($"max -> {maxDigit5}");
+
+int MaxDigit(int nu)
+{
+    int a = nu / 10;
+    int b = nu % 10;
+    // int maxDigit = a > b ? a : b;
+    // return maxDigit;
+    return a > b ? a : b;
+}
+
