d1673176f178f404631919674259a938e69aadbb
Wall-e <you@mail.com>
2023-09-28 00:14:18 +0300

    Reshenie 1 zadachi 12

---
+++ Task03(12)/Program.cs
@@ -0,0 +1,16 @@
+﻿// На вход принимает два числа
+// На выходе - кратно ли первое число второму
+// Если нет - вывести остаток от деления
+
+int a, b;
+Console.Write("Введите число a: ");
+while (!int.TryParse(Console.ReadLine(), out a))
+    Console.WriteLine("Введите число a: ");
+Console.Write("Введите число b: ");
+while (!int.TryParse(Console.ReadLine(), out b))
+    Console.WriteLine("Введите число b: ");
+int x = a % b;
+if (a % b == 0)
+    Console.WriteLine($"{a}, {b} -> Кратно");
+else
+    Console.WriteLine($"{a}, {b} -> Не кратно, остаток {x}");
