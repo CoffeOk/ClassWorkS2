fe7513444b3a8f4f6acb6bab957a105dcadca2f3
Wall-e <you@mail.com>
2023-09-28 02:05:41 +0300

    Reshenie 2 zadachi 16

---
+++ Task05(16)/Program.cs
@@ -0,0 +1,25 @@
+﻿// На вход два числа
+// Является ли одно число квадратом другого
+
+
+int a, b;
+bool Square (int x, int y)
+{
+    return x * x == y || y * y == x; 
+}
+Console.Write("Введите число a: ");
+while (!int.TryParse(Console.ReadLine(), out a))
+    Console.WriteLine("Введите число a: ");
+Console.Write("Введите число b: ");
+while (!int.TryParse(Console.ReadLine(), out b))
+    Console.WriteLine("Введите число b: ");
+bool c = Square (a,b);
+Console.WriteLine (c ? $"{a}, {b} -> да" : $"{c} -> no" );
+/* 
+if (a * a == b)
+    Console.WriteLine($"{a}, {b} -> да");
+else if (b * b == a)
+    Console.WriteLine($"{a}, {b} -> да");
+else 
+    Console.WriteLine($"{a}, {b} -> нет"); 
+*/
